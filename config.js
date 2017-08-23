2652d307cd95c1935300c7d3e03e65aef989a3b9
Santi <snroca@hotmail.com>
2017-08-23 14:17:09 -0300

    kasjd

---
+++ config.js
@@ -0,0 +1,20 @@
+var config = {
+    settings:{
+        building: 'Wellington',
+        userToken: 'YBV',
+        alphaChannel: 0.35,
+        restUrl: 'https://santiiiii.github.io',
+        socketUrl: 'wss://local.youbim.com:8443/',
+        canvasContainer: document.getElementById('YouBIMViewer'),
+        canvasId: 'viewer',
+
+        toolbar: {
+            actionBar: true,
+            systemPanel: true,
+            savedViews: true
+        }
+
+    }
+};
+
+THRESHOLD = 1;
